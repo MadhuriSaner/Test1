d02f15c933ba0d9aa3f359c260123f6a78a592d6
shind <shind@LAPTOP-3B04Q1EJ>
2022-11-02 16:47:53 +0530

    Demo Commit

---
+++ src/Practice/Demo1.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Demo1 
+{
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hi");
+		main();
+	}
+    
+	public static void main()
+	{
+		System.out.println("Hello");
+	}
+}
